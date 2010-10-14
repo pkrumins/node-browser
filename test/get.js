55dffea4c940ff7dc7887a467a45e83638cb85c8
Peteris Krumins <peteris.krumins@gmail.com>
2010-10-14 22:14:39 +0300

    added simple get test

---
+++ test/get.js
@@ -0,0 +1,20 @@
+var Browser = require('browser');
+var expresso = require('expresso');
+
+module.exports.get = function (assert) {
+    var browser = new Browser;
+    var executed = 0;
+    browser
+        .get('http://www.reddit.com')
+        .tap(function (storage, response, data) {
+            executed++;
+            assert.deepEqual(storage, {});
+            assert.ok(/reddit/.test(response.headers['set-cookie']));
+        })
+        .end();
+
+    setTimeout(function () {
+        assert.equal(executed, 1)
+    }, 1000);
+}
+
