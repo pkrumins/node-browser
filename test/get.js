f8fa08b4ceb2f2708eee389153a6c777d0c869aa
Peteris Krumins <peteris.krumins@gmail.com>
2010-10-16 00:10:14 +0300

    test for .get with opts hash

--- test/get.js
+++ test/get.js
@@ -18,3 +18,20 @@ module.exports.get = function (assert) {
     }, 2000);
 }
 
+module.exports['get with opts'] = function (assert) {
+    var browser = new Browser;
+    var executed = 0;
+    browser
+        .get({ url : 'http://www.reddit.com' })
+        .tap(function (storage, response, data) {
+            executed++;
+            assert.deepEqual(storage, {});
+            assert.ok(/reddit/.test(response.headers['set-cookie']));
+        })
+        .end();
+
+    setTimeout(function () {
+        assert.equal(executed, 1)
+    }, 2000);
+
+}
